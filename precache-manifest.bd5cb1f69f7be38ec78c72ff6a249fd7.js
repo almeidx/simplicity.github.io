6c4e6ebce56de707edc0dd659f8fadfaf1b08fcc
Almeeida <rodrigofigueire19@gmail.com>
2020-03-04 19:22:26 +0000

    Updates

---
+++ precache-manifest.bd5cb1f69f7be38ec78c72ff6a249fd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b0da47abe9ae64b50cc109061f4c29b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8de86fee28222af5b6e8",
+    "url": "/static/css/2.36b46485.chunk.css"
+  },
+  {
+    "revision": "41e2f0ed7582c7ad8da8",
+    "url": "/static/css/main.03f7a6b5.chunk.css"
+  },
+  {
+    "revision": "8de86fee28222af5b6e8",
+    "url": "/static/js/2.cbc96b54.chunk.js"
+  },
+  {
+    "revision": "41e2f0ed7582c7ad8da8",
+    "url": "/static/js/main.53de9532.chunk.js"
+  },
+  {
+    "revision": "da594b39ae80016a11b9",
+    "url": "/static/js/runtime-main.b7dac3b3.js"
+  },
+  {
+    "revision": "8dc51b789873787c246a461d8bf3844e",
+    "url": "/static/media/simplicity-logo.8dc51b78.png"
+  }
+]);
